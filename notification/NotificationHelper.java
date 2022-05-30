6efeae39d771a9ca3dd2a3219a4cfc56fe8d969d
wmartinmimi <martinmimiw@gmail.com>
2022-05-30 17:31:32 +0100

    moved NotificationHelper into MGGames utils

---
+++ notification/NotificationHelper.java
@@ -0,0 +1,54 @@
+package com.martinmimigames.util.notification;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.os.Build;
+
+public class NotificationHelper {
+
+  public static Notification createNotification(Context context, String channelId, String category) {
+    if (Build.VERSION.SDK_INT >= 26) {
+      return new Notification.Builder(context, channelId)
+          .setCategory(category)
+          .build();
+    } else {
+      return new Notification();
+    }
+  }
+
+  public static NotificationChannel setupNotificationChannel(Context context, String channelId, CharSequence name, String description, int importance) {
+    // Create Notification channel
+    if (Build.VERSION.SDK_INT >= 26) {
+      NotificationChannel channel = new NotificationChannel(channelId, name, importance);
+      channel.setDescription(description);
+      // Register the channel with the system; you can't change the importance
+      // or other notification behaviors after this
+      NotificationManager notificationManager = getNotificationManager(context);
+      notificationManager.createNotificationChannel(channel);
+      return channel;
+    }
+    return null;
+  }
+
+  public static NotificationManager getNotificationManager(Context context) {
+    return (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+  }
+
+  public static void send(Context context, int id, Notification notification) {
+    send(getNotificationManager(context), id, notification);
+  }
+
+  public static void send(NotificationManager notificationManager, int id, Notification notification) {
+    notificationManager.notify(id, notification);
+  }
+
+  public static void unsend(Context context, int id) {
+    unsend(getNotificationManager(context), id);
+  }
+
+  public static void unsend(NotificationManager notificationManager, int id) {
+    notificationManager.cancel(id);
+  }
+}
